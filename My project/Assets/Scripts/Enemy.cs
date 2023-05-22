930c0a650fd138ede482d06494f2d3d46685c371
jo3y49 <71416767+jo3y49@users.noreply.github.com>
2023-05-22 14:30:31 -0700

    bees spawn from hive

--- My project/Assets/Scripts/Enemy.cs
+++ My project/Assets/Scripts/Enemy.cs
@@ -16,7 +16,7 @@ public class Enemy : MonoBehaviour
         }
     }
 
-    public GameObject player;
+    private GameObject player;
     public float speed;
     private float distance;
     public float health = 1;
@@ -33,7 +33,7 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
