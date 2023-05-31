a9d2d64f69ffae4d9a410373fc698fce34e7c315
jo3y49 <josephmay@csus.edu>
2023-05-31 13:48:58 -0700

    refixed chest

---
+++ Witch Game/Assets/Scripts/ChestToggle.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestToggle : MonoBehaviour
+{
+    public GameObject chestCanvas;
+    public KeyCode chestKey = KeyCode.C;
+
+    private bool ischestCanvasOpen = false;
+    // Start is called before the first frame update
+    public void Start()
+    {
+        chestCanvas.SetActive(false);
+    }
+
+    // Update is called once per frame
+    public void Updatethis()
+    {
+        if(Input.GetKeyDown(chestKey))
+        {
+            ischestCanvasOpen= !ischestCanvasOpen;
+            chestCanvas.SetActive(ischestCanvasOpen);
+        }
+    }
+}
