a82caa634039d48fa2bfe3f674cfc123ae4ff596
Eazythedev <Ezaiahthedev@gmail.com>
2023-06-13 17:46:16 -0700

    Yeet

--- Witch Game/Assets/Scripts/SceneSwitch.cs
+++ Witch Game/Assets/Scripts/SceneSwitch.cs
@@ -6,7 +6,7 @@ public class SceneSwitch : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.CompareTag("Player"))
         {
-            SceneManager.LoadScene("Purgatory");
+            SceneManager.LoadScene("PurgMaze");
         }      
     }
 }
