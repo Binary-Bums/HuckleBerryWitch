7b594d6a2d4cc1be2853d01aa3dd833617913988
jo3y49 <71416767+jo3y49@users.noreply.github.com>
2023-08-18 21:21:08 -0700

    cerberus moves, is a little buggy

--- Witch Game/Assets/Scripts/Enemies/Cerberus.cs
+++ Witch Game/Assets/Scripts/Enemies/Cerberus.cs
@@ -1,19 +1,25 @@
-using System;
+using System.Collections;
 using UnityEngine;
 
 public class Cerberus : MonoBehaviour {
-    public static float maxHealth;
+    public static float maxHealth = 100f;
     private float health;
-    public float walkSpeed;
-    public float runSpeed;
+    public float walkSpeed = 5f;
+    public float walkTime = 1f;
+    public float walkCooldown = .5f;
+    public float runSpeed = 7f;
+    public float runTime = 3f;
+    public float runCooldown = 1f;
 
-    public float touchCooldown;
-    public int touchDamage;
+    public float touchCooldown = .5f;
+    public int touchDamage = 20;
     [SerializeField] private GameObject fireballPrefab;
-    public float fireballDamage = 40f;
+    public int fireballDamage = 40;
     public float fireballSpeed = 10f;
     public float fireballCooldown = 1f; // Cooldown time in seconds
     private float nextAttackTime; // When the next attack can happen
+
+    private bool acting = false;
 
     private State state = State.Neutral;
     private PlayerInfo.Direction direction = PlayerInfo.Direction.Down;
@@ -38,15 +44,13 @@ public class Cerberus : MonoBehaviour {
         nextAttackTime = Time.time + fireballCooldown; // Initialize the next attack time
         TryGetComponent(out anim);
 
+        StartCoroutine(Act());
+
     }
 
     private void Update() {
 
-        if (Time.time >= nextAttackTime) // Check if it's time to attack
-        {
-            CheckDirection();
-            Decide();
-        }
+        
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -91,19 +95,87 @@ public class Cerberus : MonoBehaviour {
         anim.SetInteger("Direction", (int)direction);
     }
 
-    private void Decide()
+    private void Decide(int actions)
     {
-        
+        int r = Random.Range(0,actions);
+
+        switch(r)
+        {
+            case 0:
+            Move(walkSpeed, walkTime, walkCooldown);
+            break;
+            case 1:
+            Jump();
+            break;
+            case 2:
+            ChargeFire();
+            break;
+        }
+
+        Debug.Log(r);
     }
 
     private void ChargeFire()
     {
+        CheckDirection();
         anim.SetBool("Fire", true);
     }
 
-    private void Move()
+    private void Move(float speed, float time, float cooldown)
     {
+        CheckDirection();
+        // Determine the direction vector based on the direction variable
+        Vector2 moveDirection = Vector2.zero;
+        switch (direction)
+        {
+            case PlayerInfo.Direction.Up:
+                moveDirection = Vector2.up;
+                break;
+            case PlayerInfo.Direction.Down:
+                moveDirection = Vector2.down;
+                break;
+            case PlayerInfo.Direction.Left:
+                moveDirection = Vector2.left;
+                break;
+            case PlayerInfo.Direction.Right:
+                moveDirection = Vector2.right;
+                break;
+        }
+
+        // Raycast in the move direction to check for barriers
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, moveDirection, 1f /* or desired distance */);
+        if (hit.collider != null && hit.collider.tag == "Barrier")
+        {
+            // Barrier detected, don't move
+            return;
+        }
+
+        // Start the movement coroutine
+        StartCoroutine(MoveCoroutine(moveDirection, speed, time, cooldown));
+    }
+
+    private IEnumerator MoveCoroutine(Vector2 moveDirection, float speed, float time, float cooldown)
+    {
+        float startTime = Time.time;
+        Vector3 startPosition = transform.position;
+        Vector3 targetPosition = transform.position + (Vector3)moveDirection * speed;
+
         anim.SetBool("Move", true);
+
+        while (Time.time < startTime + time)
+        {
+            transform.position = Vector3.Lerp(startPosition, targetPosition, (Time.time - startTime) / time);
+            yield return null;
+        }
+
+        anim.SetBool("Move", false);
+        nextAttackTime = cooldown;
+        acting = false;
+    }
+
+    private void Jump()
+    {
+        anim.SetTrigger("Jump");
     }
 
     private void Fire()
@@ -111,12 +183,33 @@ public class Cerberus : MonoBehaviour {
         Vector3 shootDirection = (player.transform.position - transform.position).normalized;
         GameObject fireball = Instantiate(fireballPrefab, transform.position, Quaternion.identity);
         fireball.GetComponent<CerbBall>().Initialize(fireballDamage, fireballSpeed, shootDirection);
-        nextAttackTime = Time.time + fireballCooldown; // Reset the attack timer
+        nextAttackTime = fireballCooldown; // Reset the attack timer
         anim.SetBool("Fire", false);
+        acting = false;
     }
 
     private void Rush()
     {
-        
+        anim.ResetTrigger("Jump");
+        Move(runSpeed, runTime, runCooldown);
+    }
+
+    private IEnumerator Act()
+    {
+        int actions = 3;
+
+        while (true)
+        {
+            Decide(actions);
+            acting = true;
+
+            while (acting)
+            {
+
+                yield return new WaitForSeconds(.5f);
+            }
+
+            yield return new WaitForSeconds(nextAttackTime);
+        }
     }
 }
